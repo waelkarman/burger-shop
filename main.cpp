87da452e1fbe7075f643da36eaedb172b33cb27c
Wael Karman <wael.karman@gmail.com>
2024-03-08 11:34:35 +0100

    init
    Signed-off-by: Wael Karman <wael.karman@gmail.com>

--- main.cpp
+++ main.cpp
@@ -16,83 +16,83 @@ using namespace std;
 int main(int argv, char** args){
 
     // Test Book class
-    Book m_book_a;
-    Book m_book_b;
-    Book m_book_c;
-    Book m_book_d;
-    Book m_book_e;
-    Book m_book_f;
-    Book m_book_g;
-    Book m_book_h;
-    Book m_book_i;
+    Book m_burger_a;
+    Book m_burger_b;
+    Book m_burger_c;
+    Book m_burger_d;
+    Book m_burger_e;
+    Book m_burger_f;
+    Book m_burger_g;
+    Book m_burger_h;
+    Book m_burger_i;
 
 
-    m_book_a.setIsdn("987255324");
-    m_book_a.setTitle("Il piccolo principe");
-    m_book_a.setPrice(55);
-    m_book_a.setBackground("./media/a1.jpg");
+    m_burger_a.setIsdn("987255324");
+    m_burger_a.setTitle("Hot Dog");
+    m_burger_a.setPrice(55);
+    m_burger_a.setBackground("./media/a1.jpg");
 
-    m_book_b.setIsdn("982355324");
-    m_book_b.setTitle("La guerra di piero");
-    m_book_b.setPrice(23);
-    m_book_b.setBackground("./media/a2.jpg");
+    m_burger_b.setIsdn("982355324");
+    m_burger_b.setTitle("Chicken Burger");
+    m_burger_b.setPrice(23);
+    m_burger_b.setBackground("./media/a2.jpg");
 
-    m_book_c.setIsdn("987267524");
-    m_book_c.setTitle("Le mille e una notte");
-    m_book_c.setPrice(15);
-    m_book_c.setBackground("./media/a3.jpg");
+    m_burger_c.setIsdn("987267524");
+    m_burger_c.setTitle("Cheeseburger");
+    m_burger_c.setPrice(15);
+    m_burger_c.setBackground("./media/a3.jpg");
 
-    m_book_d.setIsdn("987877524");
-    m_book_d.setTitle("Murakami");
-    m_book_d.setPrice(14);
-    m_book_d.setBackground("./media/a4.jpg");
+    m_burger_d.setIsdn("987877524");
+    m_burger_d.setTitle("Chicken Little");
+    m_burger_d.setPrice(14);
+    m_burger_d.setBackground("./media/a4.jpg");
 
-    m_book_e.setIsdn("987254324");
-    m_book_e.setTitle("Il cavaliere inesistente");
-    m_book_e.setPrice(5);
-    m_book_e.setBackground("./media/a5.jpg");
+    m_burger_e.setIsdn("987254324");
+    m_burger_e.setTitle("Vegan Burger");
+    m_burger_e.setPrice(5);
+    m_burger_e.setBackground("./media/a5.jpg");
 
-    m_book_f.setIsdn("923267524");
-    m_book_f.setTitle("Il giorno della civetta");
-    m_book_f.setPrice(45);
-    m_book_f.setBackground("./media/a6.jpg");
+    m_burger_f.setIsdn("923267524");
+    m_burger_f.setTitle("Quentin Tarallino");
+    m_burger_f.setPrice(45);
+    m_burger_f.setBackground("./media/a6.jpg");
 
-    m_book_g.setIsdn("987276473");
-    m_book_g.setTitle("Sherlock");
-    m_book_g.setPrice(95);
-    m_book_g.setBackground("./media/a7.jpg");
+    m_burger_g.setIsdn("987276473");
+    m_burger_g.setTitle("Central Park");
+    m_burger_g.setPrice(95);
+    m_burger_g.setBackground("./media/a7.jpg");
 
-    m_book_h.setIsdn("987287654");
-    m_book_h.setTitle("Io non ho paura");
-    m_book_h.setPrice(2);
-    m_book_h.setBackground("./media/a8.jpg");
+    m_burger_h.setIsdn("987287654");
+    m_burger_h.setTitle("Santana Burger");
+    m_burger_h.setPrice(2);
+    m_burger_h.setBackground("./media/a8.jpg");
 
-    m_book_i.setIsdn("341267524");
-    m_book_i.setTitle("Noi vogliamo tutto");
-    m_book_i.setPrice(8);
-    m_book_i.setBackground("./media/a9.jpg");
+    m_burger_i.setIsdn("341267524");
+    m_burger_i.setTitle("Beyonce Burger");
+    m_burger_i.setPrice(8);
+    m_burger_i.setBackground("./media/a9.jpg");
 
 
     Shop m_shop;
-    m_shop.insertBook(m_book_a);
-    m_shop.insertBook(m_book_a);
-    m_shop.insertBook(m_book_a);
+    m_shop.insertBook(m_burger_a);
+    m_shop.insertBook(m_burger_a);
+    m_shop.insertBook(m_burger_a);
     m_shop.fetchAllBooks();
     cout << "---------------" << endl;
-    m_shop.removeBook(m_book_a);
-    m_shop.removeBook(m_book_a);
+    m_shop.removeBook(m_burger_a);
+    m_shop.removeBook(m_burger_a);
     m_shop.fetchAllBooks();
-    m_shop.insertBook(m_book_a);
-    m_shop.insertBook(m_book_b);
-    m_shop.insertBook(m_book_c);
-    m_shop.insertBook(m_book_d);
-    m_shop.insertBook(m_book_e);
-    m_shop.insertBook(m_book_e);
-    m_shop.insertBook(m_book_e);
-    m_shop.insertBook(m_book_f);
-    m_shop.insertBook(m_book_g);
-    m_shop.insertBook(m_book_h);
-    m_shop.insertBook(m_book_i);
+    m_shop.insertBook(m_burger_a);
+    m_shop.insertBook(m_burger_b);
+    m_shop.insertBook(m_burger_c);
+    m_shop.insertBook(m_burger_d);
+    m_shop.insertBook(m_burger_e);
+    m_shop.insertBook(m_burger_e);
+    m_shop.insertBook(m_burger_e);
+    m_shop.insertBook(m_burger_f);
+    m_shop.insertBook(m_burger_g);
+    m_shop.insertBook(m_burger_h);
+    m_shop.insertBook(m_burger_i);
 
     m_shop.fetchAllBooks();
     m_shop.countAllBooks();
